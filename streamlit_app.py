c46725383e6d340746ee30ea8c80d6bf5ef41b03
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2022-12-08 11:03:49 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,4 +32,4 @@ if fruityvice_normalized.columns[0] != 'error':
   # printing the dataframe
   streamlit.dataframe(fruityvice_normalized)
 else:
-  streamlit.write(fruityvice_normalized.loc[1][error])
+  streamlit.write(fruityvice_normalized.loc[0][error])
