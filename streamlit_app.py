8dfacfa70ec765702fe3a1ef7d273058a60902ff
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2022-12-08 15:09:16 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,11 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -28,14 +33,13 @@ try:
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    if fruityvice_normalized.columns[0] != 'error':
-      fruityvice_normalized = fruityvice_normalized.set_index('name') # Changing the index from int to Fruit column
+    back_from_function = get_fruityvice_data(fruit_choice)
+    if back_from_function.columns[0] != 'error':
+      back_from_function = back_from_function.set_index('name') # Changing the index from int to Fruit column
       # printing the dataframe
-      streamlit.dataframe(fruityvice_normalized)
+      streamlit.dataframe(back_from_function)
     else:
-      error_message = fruit_choice + ' - ' + fruityvice_normalized.loc[0]['error']
+      error_message = fruit_choice + ' - ' + back_from_function.loc[0]['error']
       streamlit.error(error_message)
 except URLError as e:
   stremlit.error()
