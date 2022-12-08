5ec748d7e4acf59225bcd9e65057d3ff5026b79e
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2022-12-08 14:36:39 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,6 @@ streamlit.dataframe(my_data_row)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
 
-sql_comm = 'insert into fruit_load_list values("' + add_my_fruit + '")'
+sql_comm = "insert into fruit_load_list values('" + add_my_fruit + "')"
 streamlit.write(sql_comm)
 my_cur.execute(sql_comm)
