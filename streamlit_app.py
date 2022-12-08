8478ff708796dd83a8a4240455040108900bf8d0
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2022-12-08 14:16:47 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,10 @@ if fruityvice_normalized.columns[0] != 'error':
   streamlit.dataframe(fruityvice_normalized)
 else:
   streamlit.write(fruit_choice + ': ', fruityvice_normalized.loc[0]['error'])
+  
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
